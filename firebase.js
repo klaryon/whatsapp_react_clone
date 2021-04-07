eec824b0d0e424d40841984ca929e419cfa61d7a
klaryon <klary.ardon@gmail.com>
2021-04-07 13:21:32 +0200

    login component styled

--- firebase.js
+++ firebase.js
@@ -13,6 +13,6 @@ const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : fir
 
 const db = app.firestore();
 const auth = app.auth();
-const provider = new.firebase.auth.GoogleAuthProvider();
+const provider = new firebase.auth.GoogleAuthProvider();
 
 export { db, auth, provider };
