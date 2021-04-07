9dd77837fc7a649e92678ed1c4443137fd79e0f9
klaryon <klary.ardon@gmail.com>
2021-04-07 20:05:55 +0200

    chats collection added to firebase

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,9 +4,12 @@ import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import SearchIcon from "@material-ui/icons/Search";
 import * as EmailValidator from "email-validator";
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const Sidebar = () => {
+  const [user] = useAuthState(auth);
+
   const createChat = () => {
     const input = prompt(
       "Please enter email address for the user you wish to chat with"
@@ -15,7 +18,10 @@ const Sidebar = () => {
     if (!input) return null;
 
     if (EmailValidator.validate(input)) {
-      // We need to add the chat into de DB 'chats collection
+      // We need to add the chat into de DB 'chats collection'
+      db.collection("chats").add({
+        users: [user.email, input],
+      });
     }
   };
 
